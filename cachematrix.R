de5af2ba3496a300a9c390927a01ba3cee8fb757
Jrua84 <j.rua.montes@gmail.com>
2020-09-21 13:22:24 +0200

    agregando funcion CacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-  a<-45
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
   
   
 }
@@ -15,4 +25,45 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  ## attempt to get the inverse of the matrix stored in cache
+  cache <- x$getInverse()
+  
+  # return inverted matrix from cache if it exists
+  # else create the matrix in working environment
+  if (!is.null(cache)) {
+    message("getting cached data")
+    
+    # display matrix in console
+    return(cache)
+  }
+  
+  # create matrix since it does not exist
+  matrix <- x$get()
+  
+  # make sure matrix is square and invertible
+  # if not, handle exception cleanly
+  tryCatch( {
+    # set and return inverse of matrix
+    cache <- solve(matrix, ...)
+  },
+  error = function(e) {
+    message("Error:")
+    message(e)
+    
+    return(NA)
+  },
+  warning = function(e) {
+    message("Warning:")
+    message(e)
+    
+    return(NA)
+  },
+  finally = {
+    # set inverted matrix in cache
+    x$setMatrix(cache)
+  } )
+  
+  # display matrix in console
+  return (cache)
 }
