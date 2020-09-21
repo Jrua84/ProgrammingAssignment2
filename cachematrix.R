6fb6859a52e054c099583f29395e55adfbb46298
Jrua84 <j.rua.montes@gmail.com>
2020-09-21 13:33:36 +0200

    second signature finished

--- cachematrix.R
+++ cachematrix.R
@@ -17,7 +17,6 @@ makeCacheMatrix <- function(x = matrix()) {
        setInverse = setInverse, 
        getInverse = getInverse)
   
-  
 }
 
 
@@ -26,25 +25,15 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   
-  ## attempt to get the inverse of the matrix stored in cache
   cache <- x$getInverse()
-  
-  # return inverted matrix from cache if it exists
-  # else create the matrix in working environment
   if (!is.null(cache)) {
     message("getting cached data")
-    
-    # display matrix in console
     return(cache)
   }
-  
-  # create matrix since it does not exist
+
   matrix <- x$get()
-  
-  # make sure matrix is square and invertible
-  # if not, handle exception cleanly
+
   tryCatch( {
-    # set and return inverse of matrix
     cache <- solve(matrix, ...)
   },
   error = function(e) {
@@ -60,10 +49,7 @@ cacheSolve <- function(x, ...) {
     return(NA)
   },
   finally = {
-    # set inverted matrix in cache
     x$setMatrix(cache)
   } )
-  
-  # display matrix in console
   return (cache)
 }
